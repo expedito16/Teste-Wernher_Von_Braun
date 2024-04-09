88b9b5a220beb0acc969783a8405d94ce83a974b
Expedito <expeditopaivaneto@gmail.com>
2024-04-09 13:19:35 -0300

    criando layout da pagina de adicionar novo dispositivo, aplicando loading e realizando integracao

---
+++ src/app/pages/devices/create-device/create-device.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DevicesService } from '../devices.service';
+
+@Component({
+  selector: 'app-create-device',
+  templateUrl: './create-device.component.html',
+  styleUrls: ['./create-device.component.scss']
+})
+export class CreateDeviceComponent implements OnInit {
+  newDeviceForm!: FormGroup;
+  isLoading = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private devicesService: DevicesService
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.newDeviceForm = this.formBuilder.group({
+      deviceName: ['', Validators.required],
+      model: ['', Validators.required],
+      deviceManufacturer: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  back() {
+    this.router.navigate(['lista-dispositivos']);
+  }
+
+  save() {
+    if (this.newDeviceForm.valid) {
+      this.isLoading = true;
+
+      const deviceName = this.newDeviceForm.controls['deviceName'].value;
+      const model = this.newDeviceForm.controls['model'].value;
+      const deviceManufacturer = this.newDeviceForm.controls['deviceManufacturer'].value;
+      const description = this.newDeviceForm.controls['description'].value;
+      const newDevice = { deviceName: deviceName, model: model, deviceManufacturer: deviceManufacturer, description: description };
+
+      this.devicesService.createNewDevice(newDevice).subscribe(() => {
+        this.router.navigate(['lista-dispositivos']);
+        this.isLoading = false;
+      });
+    }
+  }
+}
+
