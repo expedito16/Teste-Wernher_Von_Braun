907a27171aefe16f5266c0c182301c49b05bb544
Expedito <expeditopaivaneto@gmail.com>
2024-04-09 13:17:41 -0300

    criando layout da pagina de lista de dispositivos, aplicando loading e realizando integracao

--- src/app/pages/devices/devices-list/devices-list.component.ts
+++ src/app/pages/devices/devices-list/devices-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DevicesService } from '../devices.service';
 
 @Component({
   selector: 'app-devices-list',
   templateUrl: './devices-list.component.html',
   styleUrls: ['./devices-list.component.scss']
 })
-export class DevicesListComponent {
+export class DevicesListComponent implements OnInit {
+  devicesList: any[] = [];
+  isLoading = false;
 
+  constructor(
+    private router: Router,
+    private devicesService: DevicesService
+  ) { }
+
+  ngOnInit(): void {
+    this.getDevicesList();
+  }
+
+  newDevice() {
+    this.router.navigate(['criar-dispositivo']);
+  }
+
+  getDevicesList() {
+    this.isLoading = true;
+    this.devicesService.listDevices().subscribe((data) => {
+      this.devicesList = data
+      this.isLoading = false;
+    });
+  }
 }
